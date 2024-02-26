64828b084fb0e962942c5d62a0bf61fefc4b15df
Pratikshya <pratikarki605@gmail.com>
2024-02-26 12:13:05 +0545

    commit ko message

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/pratikshya.jpg';
+import bg from '../../assets/pratikshya.png';
 import {Link} from 'react-scroll';
 
 const Intro=() => {
@@ -9,7 +9,7 @@ const Intro=() => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Pratikshya Karki</span> <br/>Data Science Enthusiast</span>
-                <p className="introPara">This is my project work for building a website with React so, I thought of building a ebsite that showcases my own portfolio</p>
+                <p className="introPara">This is my project work for building a website with React so, I thought of building a website that showcases my own portfolio</p>
                 <Link><button className="btn">Hire Me</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg"/>
